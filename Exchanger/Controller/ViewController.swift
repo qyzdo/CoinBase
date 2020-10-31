bc2e08082f6c7877ce9ec369a001fd4e8ce5e311
Oskar Figiel <qyzdo@outlook.com>
2020-10-31 23:13:00 +0100

    Added basic template view

--- Exchanger/Controller/ViewController.swift
+++ Exchanger/Controller/ViewController.swift
@@ -9,17 +9,64 @@ import UIKit
 
 final class ViewController: UIViewController {
 
-    var mainScreenView: MainScreenView {
-        return view as! MainScreenView
-     }
+    private let titles = ["item 1", "item 2", "item 3", "item 4", "item 5"]
+    private var inputField: UITextField!
+    private var resultField: UILabel!
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        mainScreenView.myLabel.text = "yay"
+    private var mainScreenView: MainScreenView {
+        return view as! MainScreenView
     }
 
+    // MARK: - Lifecycle methods
     override func loadView() {
         self.view = MainScreenView()
     }
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        mainScreenView.pickerView.delegate = self
+        mainScreenView.pickerView.dataSource = self
+        mainScreenView.inputValueField.addTarget(self, action: #selector(textFieldDidChange), for: .editingChanged)
+        inputField = mainScreenView.inputValueField
+        resultField = mainScreenView.resultValueField
+    }
+
+    // MARK: - User interaction methods
+    @objc private func textFieldDidChange() {
+
+        guard let number = inputField.text else {
+            print("No number")
+            resultField.text = ""
+            return
+        }
+
+        guard let numberDouble = Double(number) else {
+            print("Cant change number")
+            resultField.text = ""
+            return
+        }
+
+        convertPrice(price: numberDouble, rate: 2.01)
+    }
+
+    // MARK: - Functions
+    private func convertPrice(price: Double, rate: Double) {
+        resultField.text = String(price * rate)
+    }
+}
+
+extension ViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+
+    // MARK: - Picker view methods
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return titles.count
+    }
+
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return titles[row]
+    }
 }
--- Exchanger/View/MainScreenView.swift
+++ Exchanger/View/MainScreenView.swift
@@ -14,19 +14,97 @@ final class MainScreenView: UIView {
         self.backgroundColor = .systemBackground
         let safeArea = self.layoutMarginsGuide
 
-        addSubview(myLabel)
-        myLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
-        myLabel.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
+        addSubview(pickerView)
+        pickerView.topAnchor.constraint(equalTo: safeArea.topAnchor, constant: 5).isActive = true
+        pickerView.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
+        pickerView.widthAnchor.constraint(equalToConstant: 150).isActive = true
+        pickerView.heightAnchor.constraint(equalToConstant: 80).isActive = true
+
+        addSubview(imageView)
+        imageView.topAnchor.constraint(equalTo: pickerView.bottomAnchor, constant: 5).isActive = true
+        imageView.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
+        imageView.widthAnchor.constraint(equalToConstant: 150).isActive = true
+        imageView.heightAnchor.constraint(equalToConstant: 150).isActive = true
+
+        addSubview(currentPriceLabel)
+        currentPriceLabel.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 5).isActive = true
+        currentPriceLabel.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
+
+        addSubview(stackView)
+        stackView.topAnchor.constraint(equalTo: currentPriceLabel.bottomAnchor, constant: 5).isActive = true
+        stackView.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
+        stackView.widthAnchor.constraint(equalToConstant: 250).isActive = true
+
+        stackView.addArrangedSubview(inputValueLabel)
+        stackView.addArrangedSubview(inputValueField)
+        inputValueField.heightAnchor.constraint(equalToConstant: 35).isActive = true
+        stackView.addArrangedSubview(resultValueLabel)
+        stackView.addArrangedSubview(resultValueField)
+        resultValueField.heightAnchor.constraint(equalToConstant: 35).isActive = true
     }
 
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
     }
 
-    public var myLabel: UILabel = {
+    public var pickerView: UIPickerView = {
+        let pickerView = UIPickerView()
+        pickerView.translatesAutoresizingMaskIntoConstraints = false
+        return pickerView
+    }()
+
+    private var imageView: UIImageView = {
+        let imageView = UIImageView(image: UIImage(systemName: "photo"))
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+
+    private var currentPriceLabel: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
-        label.text = "Hello world"
+        label.textAlignment = .center
+        return label
+    }()
+
+    private var stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.alignment = .fill
+        stackView.distribution = .equalSpacing
+        return stackView
+    }()
+
+    private var inputValueLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .center
+        label.text = "$"
+        return label
+    }()
+
+    public var inputValueField: UITextField = {
+        let textField = UITextField()
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.backgroundColor = .systemGray5
+        textField.clearButtonMode = .always
+        textField.keyboardType = .decimalPad
+        return textField
+    }()
+
+    private var resultValueLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .center
+        label.text = "BTC"
+        return label
+    }()
+
+    public var resultValueField: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.backgroundColor = .systemGray5
         return label
     }()
 }
