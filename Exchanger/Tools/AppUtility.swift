c0ab8ad2d54b152cc99d9b42de9cfd55470b11a2
Oskar Figiel <qyzdo@outlook.com>
2020-11-12 00:57:52 +0100

    Added utilities for orientation lock

---
+++ Exchanger/Tools/AppUtility.swift
@@ -0,0 +1,26 @@
+//
+//  AppUtility.swift
+//  Exchanger
+//
+//  Created by Oskar Figiel on 12/11/2020.
+//
+
+import UIKit
+
+struct AppUtility {
+
+    static func lockOrientation(_ orientation: UIInterfaceOrientationMask) {
+
+        if let delegate = UIApplication.shared.delegate as? AppDelegate {
+            delegate.orientationLock = orientation
+        }
+    }
+
+    static func lockOrientation(_ orientation: UIInterfaceOrientationMask, andRotateTo rotateOrientation: UIInterfaceOrientation) {
+
+        self.lockOrientation(orientation)
+
+        UIDevice.current.setValue(rotateOrientation.rawValue, forKey: "orientation")
+        UINavigationController.attemptRotationToDeviceOrientation()
+    }
+}
